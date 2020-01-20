5604194138db655fd1cbe0fe771bf5214bbc54ca
Gabe Jackson <gabe@coffeepowered.co>
2020-01-19 19:25:44 -0500

    Add command tests

---
+++ server/command_channel_list_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/mattermost/mattermost-server/model"
+	"github.com/mattermost/mattermost-server/plugin/plugintest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestChannelListCommand(t *testing.T) {
+	api := &plugintest.API{}
+	api.On("GetTeamsForUser", mock.AnythingOfType("string"), mock.Anything, mock.Anything).Return(mockGenerateTeams(3), nil)
+	api.On("GetChannelsForTeamForUser", mock.AnythingOfType("string"), mock.Anything, mock.Anything).Return(mockGenerateChannels(3), nil)
+
+	var plugin Plugin
+	plugin.SetAPI(api)
+
+	t.Run("list channels successfully", func(t *testing.T) {
+		resp, isUserError, err := plugin.runListChannelsCommand([]string{}, &model.CommandArgs{})
+		require.NoError(t, err)
+		assert.False(t, isUserError)
+		assert.Contains(t, resp.Text, "channel-0")
+		assert.Contains(t, resp.Text, "channel-1")
+		assert.Contains(t, resp.Text, "channel-2")
+	})
+}
+
+func mockGenerateTeams(total int) []*model.Team {
+	var teams []*model.Team
+	for i := 0; i < total; i++ {
+		teams = append(teams, &model.Team{
+			Id: model.NewId(),
+		})
+
+	}
+
+	return teams
+}
+
+func mockGenerateChannels(total int) []*model.Channel {
+	var channels []*model.Channel
+	for i := 0; i < total; i++ {
+		channels = append(channels, &model.Channel{
+			Id:   model.NewId(),
+			Name: fmt.Sprintf("channel-%d", i),
+		})
+
+	}
+
+	return channels
+}
---
+++ server/command_move_thread_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/mattermost/mattermost-server/model"
+	"github.com/mattermost/mattermost-server/plugin/plugintest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMoveThreadCommand(t *testing.T) {
+	originalChannel := &model.Channel{
+		Id:   model.NewId(),
+		Name: "original-channel",
+	}
+
+	api := &plugintest.API{}
+	api.On("GetPostThread", mock.AnythingOfType("string"), mock.Anything, mock.Anything).Return(mockGeneratePostList(3, originalChannel.Id), nil)
+	api.On("GetChannelMember", mock.AnythingOfType("string"), mock.Anything, mock.Anything).Return(mockGenerateChannelMember(), nil)
+	api.On("GetChannel", mock.AnythingOfType("string"), mock.Anything, mock.Anything).Return(originalChannel, nil)
+	api.On("CreatePost", mock.Anything, mock.Anything).Return(mockGeneratePost(), nil)
+	api.On("DeletePost", mock.AnythingOfType("string"), mock.Anything, mock.Anything).Return(nil)
+
+	var plugin Plugin
+	plugin.SetAPI(api)
+
+	t.Run("no args", func(t *testing.T) {
+		resp, isUserError, err := plugin.runMoveThreadCommand([]string{}, &model.CommandArgs{ChannelId: originalChannel.Id})
+		require.NoError(t, err)
+		assert.True(t, isUserError)
+		assert.Contains(t, resp.Text, "Error: missing arguements")
+	})
+
+	t.Run("one arg", func(t *testing.T) {
+		resp, isUserError, err := plugin.runMoveThreadCommand([]string{"id1"}, &model.CommandArgs{ChannelId: originalChannel.Id})
+		require.NoError(t, err)
+		assert.True(t, isUserError)
+		assert.Contains(t, resp.Text, "Error: missing arguements")
+	})
+
+	t.Run("move thread successfully", func(t *testing.T) {
+		resp, isUserError, err := plugin.runMoveThreadCommand([]string{"id1", "id2"}, &model.CommandArgs{ChannelId: originalChannel.Id})
+		require.NoError(t, err)
+		assert.False(t, isUserError)
+		assert.Contains(t, resp.Text, "A thread with 3 posts has been moved to")
+	})
+
+	t.Run("not in thread channel", func(t *testing.T) {
+		resp, isUserError, err := plugin.runMoveThreadCommand([]string{"id1", "id2"}, &model.CommandArgs{ChannelId: model.NewId()})
+		require.NoError(t, err)
+		assert.True(t, isUserError)
+		assert.Contains(t, resp.Text, "Error: the move command must be run from the channel containing the post")
+	})
+}
+
+func mockGeneratePostList(total int, channelID string) *model.PostList {
+	postList := model.NewPostList()
+	for i := 0; i < total; i++ {
+		id := model.NewId()
+		postList.AddPost(&model.Post{
+			Id:        id,
+			ChannelId: channelID,
+		})
+		postList.AddOrder(id)
+	}
+
+	return postList
+}
+
+func mockGenerateChannelMember() *model.ChannelMember {
+	return &model.ChannelMember{
+		ChannelId: model.NewId(),
+	}
+}
+
+func mockGeneratePost() *model.Post {
+	return &model.Post{
+		Id: model.NewId(),
+	}
+}
