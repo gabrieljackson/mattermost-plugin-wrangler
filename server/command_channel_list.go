fbe2f1836419f4dc4226f724e54bda314e858cfe
Gabe Jackson <gabe@coffeepowered.co>
2020-02-11 19:17:41 -0500

    Add 'list channels' command filter flags
    These new flags allow for the filtering of the returned list of
    channels.

--- server/command_channel_list.go
+++ server/command_channel_list.go
@@ -5,9 +5,55 @@ import (
 	"strings"
 
 	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/spf13/pflag"
 )
 
+const (
+	flagTeamFilter    = "team-filter"
+	flagChannelFilter = "channel-filter"
+)
+
+type listChannelsOptions struct {
+	teamFilter    string
+	channelFilter string
+}
+
+func getListChannelsFlagSet() *pflag.FlagSet {
+	listChannelsFlagSet := pflag.NewFlagSet("list channels", pflag.ContinueOnError)
+	listChannelsFlagSet.String(flagTeamFilter, "", "A filter value that team names must contain to be shown on the list")
+	listChannelsFlagSet.String(flagChannelFilter, "", "A filter value that channel names must contain to be shown on the list")
+
+	return listChannelsFlagSet
+}
+
+func parseListChannelsArgs(args []string) (listChannelsOptions, error) {
+	var options listChannelsOptions
+
+	listChannelsFlagSet := getListChannelsFlagSet()
+	err := listChannelsFlagSet.Parse(args)
+	if err != nil {
+		return options, err
+	}
+
+	options.teamFilter, err = listChannelsFlagSet.GetString(flagTeamFilter)
+	if err != nil {
+		return options, err
+	}
+
+	options.channelFilter, err = listChannelsFlagSet.GetString(flagChannelFilter)
+	if err != nil {
+		return options, err
+	}
+
+	return options, nil
+}
+
 func (p *Plugin) runListChannelsCommand(args []string, extra *model.CommandArgs) (*model.CommandResponse, bool, error) {
+	options, err := parseListChannelsArgs(args)
+	if err != nil {
+		return nil, true, err
+	}
+
 	teams, appErr := p.API.GetTeamsForUser(extra.UserId)
 	if appErr != nil {
 		return nil, false, appErr
@@ -15,23 +61,41 @@ func (p *Plugin) runListChannelsCommand(args []string, extra *model.CommandArgs)
 
 	var msg string
 	for _, team := range teams {
-		msg += fmt.Sprintf("%s\n", team.Name)
+		if len(options.teamFilter) != 0 && !strings.Contains(team.Name, options.teamFilter) {
+			continue
+		}
 
 		channels, appErr := p.API.GetChannelsForTeamForUser(team.Id, extra.UserId, false)
 		if appErr != nil {
 			return nil, false, appErr
 		}
 
+		var filteredChannels []*model.Channel
 		for _, channel := range channels {
 			if channel.IsGroupOrDirect() {
 				continue
 			}
-			msg += fmt.Sprintf("%s - %s\n", channel.Id, channel.Name)
+			if len(options.channelFilter) != 0 && !strings.Contains(channel.Name, options.channelFilter) {
+				continue
+			}
+			filteredChannels = append(filteredChannels, channel)
 		}
-		msg += "\n"
+		if len(filteredChannels) == 0 {
+			continue
+		}
+
+		// Format filtered channel list and append.
+		newChannelGroup := fmt.Sprintf("%s\n", team.Name)
+		for _, channel := range filteredChannels {
+			newChannelGroup += fmt.Sprintf("%s - %s\n", channel.Id, channel.Name)
+		}
+		newChannelGroup = strings.TrimRight(newChannelGroup, "\n")
+		msg += codeBlock(newChannelGroup) + "\n"
 	}
 
-	msg = codeBlock(strings.TrimRight(msg, "\n"))
+	if len(msg) == 0 {
+		msg = "No results found"
+	}
 
 	return getCommandResponse(model.COMMAND_RESPONSE_TYPE_EPHEMERAL, msg), false, nil
 }
--- server/command_message_list.go
+++ server/command_message_list.go
@@ -9,30 +9,30 @@ import (
 )
 
 const (
-	flagCount            = "count"
-	minListMessagesCount = 1
-	maxListMessagesCount = 100
+	flagListMessagesCount = "count"
+	minListMessagesCount  = 1
+	maxListMessagesCount  = 100
 
-	flagTrimLength            = "trim-length"
-	minListMessagesTrimLength = 10
-	maxListMessagesTrimLength = 500
+	flagListMessagesTrimLength = "trim-length"
+	minListMessagesTrimLength  = 10
+	maxListMessagesTrimLength  = 500
 )
 
-type listOptions struct {
+type listMessagesOptions struct {
 	count      int
 	trimLength int
 }
 
 func getListMessagesFlagSet() *pflag.FlagSet {
 	listMessagesFlagSet := pflag.NewFlagSet("list messages", pflag.ContinueOnError)
-	listMessagesFlagSet.Int(flagCount, 20, fmt.Sprintf("Number of messages to return. Must be between %d and %d", minListMessagesCount, maxListMessagesCount))
-	listMessagesFlagSet.Int(flagTrimLength, 50, fmt.Sprintf("The max character count of messages listed before they are trimmed. Must be between %d and %d", minListMessagesTrimLength, maxListMessagesTrimLength))
+	listMessagesFlagSet.Int(flagListMessagesCount, 20, fmt.Sprintf("Number of messages to return. Must be between %d and %d", minListMessagesCount, maxListMessagesCount))
+	listMessagesFlagSet.Int(flagListMessagesTrimLength, 50, fmt.Sprintf("The max character count of messages listed before they are trimmed. Must be between %d and %d", minListMessagesTrimLength, maxListMessagesTrimLength))
 
 	return listMessagesFlagSet
 }
 
-func parseListMessagesArgs(args []string) (listOptions, error) {
-	var options listOptions
+func parseListMessagesArgs(args []string) (listMessagesOptions, error) {
+	var options listMessagesOptions
 
 	listMessagesFlagSet := getListMessagesFlagSet()
 	err := listMessagesFlagSet.Parse(args)
@@ -40,23 +40,23 @@ func parseListMessagesArgs(args []string) (listOptions, error) {
 		return options, err
 	}
 
-	options.count, err = listMessagesFlagSet.GetInt(flagCount)
+	options.count, err = listMessagesFlagSet.GetInt(flagListMessagesCount)
 	if err != nil {
 		return options, err
 	}
 	if options.count < minListMessagesCount || options.count > maxListMessagesCount {
-		return options, fmt.Errorf("%s (%d) must be between %d and %d", flagCount, options.count, minListMessagesCount, maxListMessagesCount)
+		return options, fmt.Errorf("%s (%d) must be between %d and %d", flagListMessagesCount, options.count, minListMessagesCount, maxListMessagesCount)
 	}
 
-	options.trimLength, err = listMessagesFlagSet.GetInt(flagTrimLength)
+	options.trimLength, err = listMessagesFlagSet.GetInt(flagListMessagesTrimLength)
 	if err != nil {
 		return options, err
 	}
 	if options.trimLength < minListMessagesTrimLength || options.trimLength > maxListMessagesTrimLength {
-		return options, fmt.Errorf("%s (%d) must be between %d and %d", flagTrimLength, options.trimLength, minListMessagesTrimLength, maxListMessagesTrimLength)
+		return options, fmt.Errorf("%s (%d) must be between %d and %d", flagListMessagesTrimLength, options.trimLength, minListMessagesTrimLength, maxListMessagesTrimLength)
 	}
 
-	return options, err
+	return options, nil
 }
 
 func (p *Plugin) runListMessagesCommand(args []string, extra *model.CommandArgs) (*model.CommandResponse, bool, error) {
