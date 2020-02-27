bc3aa96cebf2e9e70ea2a8a39935a39bb0285204
Gabe Jackson <gabe@coffeepowered.co>
2020-02-26 19:11:33 -0500

    Add root command tests

---
+++ server/command_test.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/mattermost/mattermost-server/v5/plugin"
+	"github.com/mattermost/mattermost-server/v5/plugin/plugintest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCommand(t *testing.T) {
+	context := &plugin.Context{}
+
+	commandUser := &model.User{
+		Id:    model.NewId(),
+		Email: "emaildomain.com",
+	}
+
+	api := &plugintest.API{}
+	api.On("GetUser", commandUser.Id).Return(commandUser, nil)
+	api.On("GetUser", mock.AnythingOfType("string"), mock.Anything, mock.Anything).Return(nil, &model.AppError{DetailedError: "invalid user"})
+
+	var plugin Plugin
+	plugin.SetAPI(api)
+
+	t.Run("no args", func(t *testing.T) {
+		args := &model.CommandArgs{}
+		resp, appErr := plugin.ExecuteCommand(context, args)
+		require.Nil(t, appErr)
+		require.Equal(t, resp.Text, getHelp())
+	})
+
+	t.Run("one arg", func(t *testing.T) {
+		args := &model.CommandArgs{Command: "one"}
+		resp, appErr := plugin.ExecuteCommand(context, args)
+		require.Nil(t, appErr)
+		require.Equal(t, resp.Text, getHelp())
+	})
+
+	t.Run("two args, invalid command", func(t *testing.T) {
+		args := &model.CommandArgs{Command: "one two"}
+		resp, appErr := plugin.ExecuteCommand(context, args)
+		require.Nil(t, appErr)
+		require.Equal(t, resp.Text, getHelp())
+	})
+
+	t.Run("move command, missing extra args", func(t *testing.T) {
+		args := &model.CommandArgs{Command: "wrangler move"}
+		resp, appErr := plugin.ExecuteCommand(context, args)
+		require.Nil(t, appErr)
+		require.Equal(t, resp.Text, getHelp())
+	})
+
+	t.Run("list command, missing extra args", func(t *testing.T) {
+		args := &model.CommandArgs{Command: "wrangler list"}
+		resp, appErr := plugin.ExecuteCommand(context, args)
+		require.Nil(t, appErr)
+		require.Equal(t, resp.Text, getHelp())
+	})
+
+	t.Run("info command", func(t *testing.T) {
+		args := &model.CommandArgs{Command: "wrangler info"}
+		resp, appErr := plugin.ExecuteCommand(context, args)
+		require.Nil(t, appErr)
+		infoResp, userError, err := plugin.runInfoCommand([]string{}, nil)
+		require.NoError(t, err)
+		assert.False(t, userError)
+		assert.Equal(t, resp, infoResp)
+	})
+
+	t.Run("allowed email domain", func(t *testing.T) {
+		t.Run("enabled, user not in domain", func(t *testing.T) {
+			plugin.setConfiguration(&configuration{
+				AllowedEmailDomain: "baddomain.com",
+			})
+			args := &model.CommandArgs{
+				UserId:  commandUser.Id,
+				Command: "wrangler info",
+			}
+			resp, appErr := plugin.ExecuteCommand(context, args)
+			require.Nil(t, appErr)
+			assert.Equal(t, resp.Text, "Permission denied. Please talk to your system administrator to get access.")
+		})
+
+		t.Run("enabled, user in domain", func(t *testing.T) {
+			plugin.setConfiguration(&configuration{
+				AllowedEmailDomain: "emaildomain.com",
+			})
+			args := &model.CommandArgs{
+				UserId:  commandUser.Id,
+				Command: "wrangler info",
+			}
+			resp, appErr := plugin.ExecuteCommand(context, args)
+			require.Nil(t, appErr)
+			infoResp, userError, err := plugin.runInfoCommand([]string{}, nil)
+			require.NoError(t, err)
+			assert.False(t, userError)
+			assert.Equal(t, resp, infoResp)
+		})
+
+		t.Run("enabled, invalid user", func(t *testing.T) {
+			plugin.setConfiguration(&configuration{
+				AllowedEmailDomain: "emaildomain.com",
+			})
+			args := &model.CommandArgs{
+				UserId:  model.NewId(),
+				Command: "wrangler info",
+			}
+			_, appErr := plugin.ExecuteCommand(context, args)
+			require.Error(t, appErr)
+		})
+	})
+}
