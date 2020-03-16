f92dd4e487ad98242db87d5201ee9b9d4041a8bc
Gabe Jackson <gabe@coffeepowered.co>
2020-03-16 09:03:26 -0400

    Update build tooling and dependencies
    The plugin Makefile and build tooling has been updated with the
    latest improvements from the Mattermost plugin starter template.

--- build/custom.mk
+++ build/custom.mk
@@ -1,1 +1,8 @@
 # Include custom targets and environment variables here
+
+BUILD_DATE = $(shell date -u)
+BUILD_HASH = $(shell git rev-parse HEAD)
+BUILD_HASH_SHORT = $(shell git rev-parse --short HEAD)
+LDFLAGS += -X "main.BuildDate=$(BUILD_DATE)"
+LDFLAGS += -X "main.BuildHash=$(BUILD_HASH)"
+LDFLAGS += -X "main.BuildHashShort=$(BUILD_HASH_SHORT)"
