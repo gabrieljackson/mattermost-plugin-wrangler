634498be5630bca62dd5c4c242474db2860a4df0
Gabe Jackson <gabe@coffeepowered.co>
2020-04-19 18:17:43 -0400

    Send DM to user when a message is attached to a thread (#47)
    This is only done when the user running the command didn't create
    the original message.

--- server/bot.go
+++ server/bot.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 
 	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/pkg/errors"
 )
 
 // PostBotDM posts a DM as the Wrangler bot user.
 func (p *Plugin) PostBotDM(userID, message string) error {
 	channel, appError := p.API.GetDirectChannel(userID, p.BotUserID)
 	if appError != nil {
-		return appError
+		return errors.Wrap(appError, "unable to get direct channel")
 	}
 	if channel == nil {
 		return fmt.Errorf("could not get direct channel for bot and user_id=%s", userID)
@@ -21,8 +22,11 @@ func (p *Plugin) PostBotDM(userID, message string) error {
 		ChannelId: channel.Id,
 		Message:   message,
 	})
+	if appError != nil {
+		return errors.Wrap(appError, "unable to create new post")
+	}
 
-	return appError
+	return nil
 }
 
 // PostToChannelByIDAsBot posts a message to the provided channel.
