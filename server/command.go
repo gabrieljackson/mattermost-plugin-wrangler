5a8f7f6cdd8085f234a666fd0655029fe5a2e592
Gabe Jackson <gabe@coffeepowered.co>
2020-06-21 18:55:46 -0400

    Add slash command autocomplete functionality (#75)

--- server/command.go
+++ server/command.go
@@ -14,7 +14,7 @@ const helpText = `Wrangler Plugin - Slash Command Help
 
 %s
 
-/wrangler attach message [MESSAGE_ID_TO_BE_ATTACHED] [ROOT_MESSAGE_ID]
+/wrangler attach message [MESSAGE_ID_TO_ATTACH] [ROOT_MESSAGE_ID]
   Attach a given message to a thread in the same channel
     - Obtain the message IDs by running '/wrangler list messages' or via the 'Permalink' message dropdown option (it's the last part of the URL)
 
@@ -45,8 +45,9 @@ func getCommand(autocomplete bool) *model.Command {
 		DisplayName:      "Wrangler",
 		Description:      "Manage Mattermost messages!",
 		AutoComplete:     autocomplete,
-		AutoCompleteDesc: "Available commands: move thread, attach message, list messages, list channels, info",
+		AutoCompleteDesc: "Available commands: move thread, copy thread, attach message, list messages, list channels, info",
 		AutoCompleteHint: "[command]",
+		AutocompleteData: getAutocompleteData(),
 	}
 }
 
@@ -165,4 +166,44 @@ func (p *Plugin) authorizedPluginUser(userID string) bool {
 	}
 
 	return true
+}
+
+func getAutocompleteData() *model.AutocompleteData {
+	wrangler := model.NewAutocompleteData("wrangler", "[command]", "Available commands: move, copy, attach, list, info, help")
+
+	move := model.NewAutocompleteData("move", "[subcommand]", "Move messages")
+	moveThread := model.NewAutocompleteData("thread", "[MESSAGE_ID] [CHANNEL_ID]", "Move a message and the thread it belongs to")
+	moveThread.AddTextArgument("The ID of the message to be moved", "[MESSAGE_ID]", "")
+	moveThread.AddTextArgument("The ID of the channel where the message will be moved to", "[CHANNEL_ID]", "")
+	move.AddCommand(moveThread)
+	wrangler.AddCommand(move)
+
+	copy := model.NewAutocompleteData("copy", "[subcommand]", "Copy messages")
+	copyThread := model.NewAutocompleteData("thread", "[MESSAGE_ID] [CHANNEL_ID]", "Copy a message and the thread it belongs to")
+	copyThread.AddTextArgument("The ID of the message to be copied", "[MESSAGE_ID]", "")
+	copyThread.AddTextArgument("The ID of the channel where the message will be copied to", "[CHANNEL_ID]", "")
+	copy.AddCommand(copyThread)
+	wrangler.AddCommand(copy)
+
+	attach := model.NewAutocompleteData("attach", "[subcommand]", "Attach messages")
+	attachMessage := model.NewAutocompleteData("message", "[MESSAGE_ID_TO_ATTACH] [ROOT_MESSAGE_ID]", "Attach a message to a thread in the channel")
+	attachMessage.AddTextArgument("The ID of the message to be attached", "[MESSAGE_ID_TO_ATTACH]", "")
+	attachMessage.AddTextArgument("The root message ID of the thread", "[ROOT_MESSAGE_ID]", "")
+	attach.AddCommand(attachMessage)
+	wrangler.AddCommand(attach)
+
+	list := model.NewAutocompleteData("list", "[subcommand]", "Lists IDs for channels and messages")
+	listChannels := model.NewAutocompleteData("channels", "[optional flags]", "List channel IDs that you have joined")
+	listMessages := model.NewAutocompleteData("messages", "[optional flags]", "List message IDs in this channel")
+	list.AddCommand(listChannels)
+	list.AddCommand(listMessages)
+	wrangler.AddCommand(list)
+
+	info := model.NewAutocompleteData("info", "", "Shows plugin information")
+	wrangler.AddCommand(info)
+
+	help := model.NewAutocompleteData("help", "", "Shows detailed help information")
+	wrangler.AddCommand(help)
+
+	return wrangler
 }
