8338b7882bb75438915ec0c5696825ec00184de0
Gabe Jackson <3694686+gabrieljackson@users.noreply.github.com>
2023-09-30 13:22:08 -0400

    Fix webapp builds
    This corrects some npm build issues while also bumping some
    dependencies.

--- Makefile
+++ Makefile
@@ -45,7 +45,7 @@ endif
 
 ## Runs go style checks.
 .PHONY: check-style-go
-check-style-go: gofmt govet golint
+check-style-go: gofmt golint
 
 ## Runs gofmt against all packages.
 .PHONY: gofmt
--- webapp/src/components/left_sidebar_element/index.ts
+++ webapp/src/components/left_sidebar_element/index.ts
@@ -1,13 +1,13 @@
 import {connect} from 'react-redux';
 
 import {GlobalState} from 'mattermost-redux/types/store';
-import {getNewSidebarPreference} from 'mattermost-redux/selectors/entities/preferences';
+import {getSidebarPreferences} from 'mattermost-redux/selectors/entities/preferences';
 
 import LeftSidebarElement from './left_sidebar_element';
 
 function mapStateToProps(state: GlobalState) {
     return {
-        newSidebar: getNewSidebarPreference(state),
+        newSidebar: getSidebarPreferences(state),
     };
 }
 
